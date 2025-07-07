37929b8391177070ae1b4f27452259b5f33a8be6
Raine040 <raineeimre@hotmail.com>
2025-07-07 08:46:11 -0400

    db connectivity

--- server/db.js
+++ server/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const mongoURI =  "mongodb://root:<your-password>@127.0.0.1:27017";
+const mongoURI =  "mongodb://root:pWaY2RG9WewydDE9LkPntRnu@172.21.152.40:27017";
 
 const connectToMongo = async (retryCount) => {
     const MAX_RETRIES = 3;
